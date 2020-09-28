bb9f9ded27969e9e83a9e9dff687e503634b5875
ELORCHI ELMEHDI <Pythagoras@localhost.localdomain>
2020-09-28 21:29:29 +0100

    deep

--- srcs/download.sh
+++ srcs/download.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 apt-get update
+apt-get install -y wget
+apt-get install -y dpkg
 
-apt-get install ngnix -y
 
-apt-get install wget -y
 
-apt-get install gnupg -y
 
-apt-get install vim -y
+#install mysql
 
+export DEBIAN_FRONTEND=nonintercative
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+##
 

3cf89139f30b4d9033f5ba54d5a51d0d87007269
El mehdi El orchi <eel-orch@e2p12p12.1337.ma. 12.12.12.10.in-addr.arpa name = e2r12p12.1337.ma>
2020-10-19 17:06:12 +0100

    wlm ngnix

--- server_khaf/srcs/restore_website.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE wordpress; use wordpress; source ~/wpdb.sql;
-GRANT ALL PRIVILEGES ON wordpress.* TO khafni@localhost; FLUSH PRIVILEGES;
--- server_khaf/srcs/services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service nginx stop
-service mysql stop
-service php7.3-fpm stop
-
-service nginx start
-service mysql start
-service php7.3-fpm start

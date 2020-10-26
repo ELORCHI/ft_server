8546afffdf2bae576ede33cdeae9721fdd7748de
El mehdi El orchi <eel-orch@e2r12p6.1337.ma>
2020-10-26 14:11:19 +0100

    mysql

--- test/srcs/install_phpmyadmin.sh
+++ test/srcs/install_phpmyadmin.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 cd /tmp
-
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-all-languages.tar.gz
-
 mkdir /var/www/html/phpmyadmin
-
 tar xzf phpMyAdmin-4.9.7-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpmyadmin
-
 rm *.gz
 rm -Rf *;
-
-cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
-
+rm /var/www/html/phpmyadmin/config.sample.inc.php
+mv /config.inc.php /var/www/html/phpmyadmin/
+mv /default /etc/nginx/sites-available/default
 chmod 660 /var/www/html/phpmyadmin/config.inc.php
 
 chown -R www-data:www-data /var/www/html/phpmyadmin
